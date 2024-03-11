5585c2fd18f080b2dc692839a65b074ecd04e23e
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-11 14:51:06 -0300

    Middleware ajustado!

--- src/service/api.js
+++ src/service/api.js
@@ -1,37 +1,43 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import axios from 'axios'
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-})
+  baseURL: 'http://localhost:3333',
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+  },
+});
 
-export const createAccount = async ({ profile, email, name, birthday, cellphone, password }) => 
-    api.post('/account', {
-        profile: `${profile}`,
-        email: `${email}`,
-        name: `${name}`,
-        birthday: `${birthday}`,
-        cellphone: `${cellphone}`,
-        password: `${password}`
-        // Outros dados que você deseja enviar no corpo da requisição
-      })
-      .then(response => {
-        // Lida com a resposta da requisição de sucesso
-        console.log(response.data);
-      })
-      .catch(error => {
-        // Lida com o erro da requisição
-        console.error('Erro ao fazer a requisição:', error);
-      });
+export const createAccount = async ({ profile, email, name, birthday, cellphone, password }) =>
+  api
+    .post('/account', {
+      profile: `${profile}`,
+      email: `${email}`,
+      name: `${name}`,
+      birthday: `${birthday}`,
+      cellphone: `${cellphone}`,
+      password: `${password}`,
+      // Outros dados que você deseja enviar no corpo da requisição
+    })
+    .then((response) => {
+      // Lida com a resposta da requisição de sucesso
+      console.log(response.data);
+    })
+    .catch((error) => {
+      // Lida com o erro da requisição
+      console.error('Erro ao fazer a requisição:', error);
+    });
 
-      export const verifyToken = (token) => api.get(`/user/verify-email?token=${token}`)
-          .then(response => {
-            // Lida com a resposta da requisição de sucesso
-            console.log(response.data);
-            return response; // Retorna o response da requisição
-          })
-          .catch(error => {
-            // Lida com o erro da requisição
-            console.error('Erro ao fazer a requisição:', error);
-            throw error; // Lança o erro para ser tratado no componente que chama a função
-          });
+export const verifyToken = async (token) =>
+  api
+    .get(`/user/verify-email?token=${token}`)
+    .then((response) => {
+      // Lida com a resposta da requisição de sucesso
+      console.log(response.data);
+      return response; // Retorna o response da requisição
+    })
+    .catch((error) => {
+      // Lida com o erro da requisição
+      console.error('Erro ao fazer a requisição:', error);
+      throw error; // Lança o erro para ser tratado no componente que chama a função
+    });
