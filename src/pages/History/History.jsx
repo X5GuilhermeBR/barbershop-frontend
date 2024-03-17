22c8a996e1921da994b794aa17a7aed475f0e31d
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-17 11:12:14 -0300

    Calendário ajustado por completo

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import HistoryIcon from '@mui/icons-material/History';
 import { Container, Grid, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
@@ -13,6 +14,15 @@ function History() {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [noAppointments, setNoAppointments] = useState(false);
+  const [maxMonth, setMaxMonth] = useState('');
+
+  useEffect(() => {
+    // Define o mês máximo como o mês atual
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    setMaxMonth(`${year}-${month}`);
+  }, []);
 
   useEffect(() => {
     async function fetchScheduledAppointments() {
@@ -48,10 +58,8 @@ function History() {
 
   const handleDateChange = (event) => {
     const selectedMonthYear = event.target.value;
-    const [year, month] = selectedMonthYear.split('-');
-    const selectedDate = new Date(year, month - 1); // Mês - 1 porque os meses são indexados de 0 a 11
-    const currentDate = new Date();
-    if (selectedDate <= currentDate) {
+    if (selectedMonthYear <= maxMonth) {
+      const [year, month] = selectedMonthYear.split('-');
       const firstDay = `${year}-${month}-01`;
       const lastDay = new Date(year, month, 0).getDate();
       const lastDayOfMonth = `${year}-${month}-${lastDay}`;
@@ -59,6 +67,42 @@ function History() {
       setEndDate(lastDayOfMonth);
     }
   };
+
+  // Função para formatar o dia da semana, a data, o mês e o ano
+  const formatWeekdayDateMonthYear = (dateString) => {
+    const daysOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    const months = [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro',
+    ];
+    const date = new Date(dateString);
+    const weekday = daysOfWeek[date.getDay()];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const formattedDate = date.toLocaleDateString('pt-BR', { day: '2-digit' });
+    return `${weekday} - ${formattedDate} de ${month} de ${year}`;
+  };
+
+  // Agrupa os agendamentos por dia da semana
+  const groupedAppointments = scheduledAppointments.reduce((grouped, appointment) => {
+    const date = new Date(appointment.date);
+    const dayOfWeek = date.getDay();
+    if (!grouped[dayOfWeek]) {
+      grouped[dayOfWeek] = [];
+    }
+    grouped[dayOfWeek].push(appointment);
+    return grouped;
+  }, {});
 
   return (
     <>
@@ -74,7 +118,7 @@ function History() {
       >
         <Container>
           <Grid container direction="column" style={{ marginBottom: '5rem', flex: '1 0 auto' }}>
-            <Grid item xs={12} style={{ width: '100%' }}>
+            <Grid item xs={12}>
               <TextField
                 id="date"
                 label="Filtrar por Mês e Ano"
@@ -85,19 +129,30 @@ function History() {
                 }}
                 fullWidth // Ocupa todo o tamanho disponível horizontalmente
                 value={startDate.slice(0, 7)} // Ajusta para exibir apenas o ano e o mês
+                inputProps={{
+                  max: maxMonth, // Define o mês máximo
+                }}
               />
             </Grid>
-            {noAppointments ? (
+            {Object.keys(groupedAppointments).map((dayOfWeek) => (
+              <Grid item xs={12} key={dayOfWeek}>
+                <Typography variant="h6" style={{ marginBottom: '1rem' }}>
+                  {formatWeekdayDateMonthYear(groupedAppointments[dayOfWeek][0].date)}
+                </Typography>
+                <Grid container spacing={2}>
+                  {groupedAppointments[dayOfWeek].map((appointment) => (
+                    <Grid item key={appointment.id} xs={12}>
+                      <SchedulingCard appointment={appointment} />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Grid>
+            ))}
+            {noAppointments && (
               <Grid container justifyContent="center">
                 <Typography variant="h6">
                   Você não possui histórico de atendimento neste mês.
                 </Typography>
-              </Grid>
-            ) : (
-              <Grid container spacing={2} justifyContent="center">
-                {scheduledAppointments.map((appointment) => (
-                  <SchedulingCard key={appointment.id} appointment={appointment} />
-                ))}
               </Grid>
             )}
           </Grid>
