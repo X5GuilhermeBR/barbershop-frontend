ca6c7480061b83e16569efa929e24a9dcf73c8cb
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-17 19:24:21 -0300

    Criado rota para buscar um atendimento por Id.

--- src/components/SchedulingCard/SchedulingCard.js
+++ src/components/SchedulingCard/SchedulingCard.js
@@ -1,8 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Card, CardContent, CardActions, Grid, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function SchedulingCard({ appointment }) {
+  const navigate = useNavigate();
+
+  const handleEditClick = () => {
+    // Redirecionar para a página de agendamento com o ID do usuário
+    navigate(`/agendar?scheduleId=${appointment.id}`);
+    console.log(appointment, "aqio")
+  };
+
   return (
     <Grid item xs={12} sm={6} md={4}>
       <Card variant="outlined">
@@ -20,6 +29,11 @@ function SchedulingCard({ appointment }) {
             <strong>Barbeiro:</strong> {appointment.barber_name}
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" onClick={handleEditClick}>
+            Editar
+          </Button>
+        </CardActions>
       </Card>
     </Grid>
   );
