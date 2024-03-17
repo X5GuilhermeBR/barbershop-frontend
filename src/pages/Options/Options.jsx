34d170444aedba6f19849ac61a8b28720e2479fd
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-17 10:19:40 -0300

    Ajustado as opções de menu

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,33 +1,41 @@
-import CardGiftcardIcon from '@mui/icons-material/CardGiftcard'
-import ExitToAppIcon from '@mui/icons-material/ExitToApp'
-import HistoryIcon from '@mui/icons-material/History'
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
-import PasswordIcon from '@mui/icons-material/Password'
-import SettingsIcon from '@mui/icons-material/Settings'
-import { Grid } from '@mui/material'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import * as React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import FooterNavigation from '../../components/FooterNavigation/FooterNavigation'
-import PageHeader from '../../components/Header/Header'
-import { useAuth } from '../../context/AuthContext'
-
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import HistoryIcon from '@mui/icons-material/History';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import PasswordIcon from '@mui/icons-material/Password';
+import SettingsIcon from '@mui/icons-material/Settings';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Grid } from '@mui/material';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import * as React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import FooterNavigation from '../../components/FooterNavigation/FooterNavigation';
+import PageHeader from '../../components/Header/Header';
+import { useAuth } from '../../context/AuthContext';
 
 export default function Options() {
-  const { logout } = useAuth(); // Obtém a função logout do contexto de autenticação
+  const { logout, userInfo } = useAuth(); // Obtém a função logout do contexto de autenticação
   const navigate = useNavigate(); // Obtém a função de navegação
 
   const handleLogout = () => {
     if (logout()) {
-      navigate('/'); // Redireciona para a página inicial após o logout
+      navigate('/');
     }
+  };
+
+  const handleWhatsAppClick = () => {
+    const phoneNumber = '5521972862906';
+    const message = encodeURIComponent('mensagem');
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+
+    window.location.href = whatsappUrl;
   };
 
   return (
     <>
-      <PageHeader icon={<SettingsIcon/>}  title='Configurações' />
+      <PageHeader icon={<SettingsIcon />} title="Configurações" />
       <Grid item style={{ marginBottom: '4rem', marginTop: '1rem', flex: '1 0 auto', zIndex: 1 }}>
         <ListItemButton component={Link} to="/configuracoes/editar-perfil">
           <ListItemIcon>
@@ -47,13 +55,51 @@ export default function Options() {
           </ListItemIcon>
           <ListItemText primary="Meu Histórico" />
         </ListItemButton>
-        <ListItemButton component={Link} to="/loyalty-card" disabled> {/* Desabilita o clique */}
-          <ListItemIcon>
-            <CardGiftcardIcon />
-          </ListItemIcon>
-          <ListItemText primary="Cartão Fidelidade" />
-        </ListItemButton>
-        <ListItemButton onClick={handleLogout}> {/* Chama a função handleLogout ao clicar */}
+        {userInfo.profile === 'client' && (
+          <ListItemButton component={Link} to="/loyalty-card" disabled>
+            <ListItemIcon>
+              <CardGiftcardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Cartão Fidelidade" />
+          </ListItemButton>
+        )}
+        {userInfo.profile === 'barber' && (
+          <>
+            <ListItemButton component={Link} to="/configuracoes/historico" disabled>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Clientes" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/configuracoes/historico" disabled>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Produtos" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/configuracoes/historico" disabled>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Serviços" />
+            </ListItemButton>
+            <ListItemButton component={Link} to="/configuracoes/historico" disabled>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Extrato" />
+            </ListItemButton>
+          </>
+        )}
+        {userInfo.profile === 'client' && (
+          <ListItemButton onClick={handleWhatsAppClick}>
+            <ListItemIcon>
+              <WhatsAppIcon />
+            </ListItemIcon>
+            <ListItemText primary="Fale Conosco" />
+          </ListItemButton>
+        )}
+        <ListItemButton onClick={handleLogout}>
           <ListItemIcon>
             <ExitToAppIcon />
           </ListItemIcon>
