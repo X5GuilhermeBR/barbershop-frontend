3faafaf5b9df73b14f7644849266fcbdf89e2fef
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-17 23:09:37 -0300

    Ajustado o card para exibir valor do serviço

--- src/components/SchedulingCard/SchedulingCard.js
+++ src/components/SchedulingCard/SchedulingCard.js
@@ -1,13 +1,55 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { Card, CardContent, CardActions, Grid, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardContent, CardActions, Grid, Typography, Button, Rating, Snackbar, Alert } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { updateSchedule } from '../../service/api';
 
 function SchedulingCard({ appointment }) {
   const navigate = useNavigate();
+  const [userRating, setUserRating] = useState(appointment.rating || null);
+  const [isRatingEditable, setIsRatingEditable] = useState(appointment.rating === null);
+  const [isRatingSelected, setIsRatingSelected] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success'); // 'success' or 'error'
 
   const handleEditClick = () => {
-    navigate(`/agendar?scheduleId=${appointment.id}`);
+    if (appointment.status === 'Agendado') {
+      navigate(`/agendar?scheduleId=${appointment.id}`);
+    }
+  };
+
+  const handleRatingChange = (event, newValue) => {
+    setUserRating(newValue);
+    setIsRatingSelected(true); // Habilitar o botão de avaliação quando o usuário selecionar uma quantidade de estrelas
+  };
+
+  const handleSaveRating = async () => {
+    if (appointment.status === 'Finalizado') {
+      try {
+        await updateSchedule(appointment.id, { rating: userRating });
+        setSnackbarMessage('Avaliação salva com sucesso!');
+        setSnackbarSeverity('success');
+        setSnackbarOpen(true);
+        setIsRatingEditable(false);
+      } catch (error) {
+        console.error('Erro ao salvar avaliação:', error);
+        setSnackbarMessage('Erro ao salvar avaliação. Tente novamente mais tarde.');
+        setSnackbarSeverity('error');
+        setSnackbarOpen(true);
+      }
+    } else {
+      setSnackbarMessage("Você só pode avaliar agendamentos com status 'Finalizado'");
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+    }
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
   };
 
   return (
@@ -29,17 +71,53 @@ function SchedulingCard({ appointment }) {
             <strong>Status:</strong> {appointment.status}
             <br />
             <strong>Valor:</strong> R${appointment.service_price}
-            
           </Typography>
         </CardContent>
-        {appointment.status === 'Agendado' && (
         <CardActions>
-          <Button size="small" color="primary" onClick={handleEditClick}>
-            Editar
-          </Button>
+          {isRatingEditable && appointment.status === 'Finalizado' && (
+            <>
+              <Rating
+                name={`rating-${appointment.id}`}
+                value={userRating}
+                onChange={handleRatingChange}
+                max={5} // Definindo o número máximo de estrelas como 5
+              />
+              {isRatingSelected && ( // Exibir o botão de avaliação somente quando uma quantidade de estrelas for selecionada
+                <Button size="small" color="primary" onClick={handleSaveRating}>
+                  Avaliar
+                </Button>
+              )}
+            </>
+          )}
+          {!isRatingEditable && appointment.status === 'Finalizado' && (
+            <Rating
+              name={`rating-${appointment.id}`}
+              value={userRating}
+              readOnly
+              max={5}
+            />
+          )}
+          {appointment.status === 'Agendado' && (
+            <Button size="small" color="primary" onClick={handleEditClick}>
+              Editar
+            </Button>
+          )}
         </CardActions>
-        )}
       </Card>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity={snackbarSeverity}
+          sx={{ width: '100%' }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Grid>
   );
 }
