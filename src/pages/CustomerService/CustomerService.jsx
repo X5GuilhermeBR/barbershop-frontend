2728b1bc604e8bef9ed113639e7ffddaae045016
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-28 00:46:55 -0300

    WIP: CustomerService faltando apenas cadastro de produtos consumidos

--- src/pages/CustomerService/CustomerService.jsx
+++ src/pages/CustomerService/CustomerService.jsx
@@ -1,8 +1,9 @@
-import { Button, Container, Grid, Typography } from '@mui/material';
+/* eslint-disable no-nested-ternary */
+import { Button, CircularProgress, Container, Grid, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
-import { getScheduleById } from '../../service/api';
+import { getScheduleById, updateSchedule } from '../../service/api';
 
 function CustomerService() {
   const [clientName, setClientName] = useState('');
@@ -10,12 +11,14 @@ function CustomerService() {
   const [scheduledTime, setScheduledTime] = useState('');
   const [scheduledDate, setScheduledDate] = useState('');
   const [scheduleService, setScheduleService] = useState('');
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [scheduleStatus, setScheduleStatus] = useState('');
   const navigate = useNavigate();
   const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const scheduleId = params.get('scheduleId');
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const scheduleId = params.get('scheduleId');
     if (scheduleId) {
       getScheduleById(scheduleId)
         .then((response) => {
@@ -25,6 +28,7 @@ function CustomerService() {
           setScheduledTime(scheduleData.time);
           setScheduledDate(scheduleData.date);
           setScheduleService(scheduleData.serviceName);
+          setScheduleStatus(scheduleData.status);
         })
         .catch((error) => {
           console.error('Error fetching schedule:', error);
@@ -34,6 +38,41 @@ function CustomerService() {
       navigate('/agenda');
     }
   }, [location.search, navigate]);
+
+  const handleStartService = async () => {
+    setIsProcessing(true);
+    try {
+      const updatedSchedule = await updateSchedule(scheduleId, {
+        status: 'Em Andamento',
+        started_service: new Date(),
+      });
+      console.log('Atendimento iniciado:', updatedSchedule);
+      setIsProcessing(false);
+    } catch (error) {
+      console.error('Erro ao iniciar o atendimento:', error);
+      setIsProcessing(false);
+    }
+  };
+
+  const handleFinishService = async () => {
+    setIsProcessing(true);
+    try {
+      const updatedSchedule = await updateSchedule(scheduleId, {
+        status: 'Finalizado',
+        finished_service: new Date(),
+      });
+      console.log('Atendimento finalizado:', updatedSchedule);
+      setIsProcessing(false);
+    } catch (error) {
+      console.error('Erro ao finalizar o atendimento:', error);
+      setIsProcessing(false);
+    }
+  };
+
+  const handleEditService = () => {
+    // Adicione aqui a lógica para redirecionar para a página de edição de atendimento
+    navigate(`/editar-atendimento/${scheduleId}`);
+  };
 
   return (
     <>
@@ -49,7 +88,7 @@ function CustomerService() {
       >
         <Container maxWidth="sm">
           <Typography variant="h6" style={{ marginBottom: '1rem' }}>
-            Detalhes do Serviço
+            Detalhes do Serviço - Status: {scheduleStatus}
           </Typography>
           <Typography variant="body1" style={{ marginBottom: '1rem' }}>
             Cliente: {clientName}
@@ -63,9 +102,41 @@ function CustomerService() {
           <Typography variant="body1" style={{ marginBottom: '1rem' }}>
             Serviço: {scheduleService}
           </Typography>
-          <Button variant="contained" color="primary" fullWidth style={{ marginTop: '1rem' }}>
-            Iniciar Atendimento
-          </Button>
+          {isProcessing ? (
+            <Button disabled fullWidth style={{ marginTop: '1rem' }}>
+              <CircularProgress size={24} /> Processando...
+            </Button>
+          ) : scheduleStatus === 'Em Andamento' ? (
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleFinishService}
+              fullWidth
+              style={{ marginTop: '1rem' }}
+            >
+              Finalizar Atendimento
+            </Button>
+          ) : scheduleStatus === 'Finalizado' ? (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleEditService}
+              fullWidth
+              style={{ marginTop: '1rem' }}
+            >
+              Editar Atendimento
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleStartService}
+              fullWidth
+              style={{ marginTop: '1rem' }}
+            >
+              Iniciar Atendimento
+            </Button>
+          )}
         </Container>
       </Grid>
     </>
