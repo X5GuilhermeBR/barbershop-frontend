d415b3b2dffed16d70caee633a7b789566486484
Guilherme José <gui_guifonseca@hotmail.com>
2024-03-31 22:32:09 -0300

    WIP: CSS CustomerService

--- src/pages/CustomerService/CustomerService.jsx
+++ src/pages/CustomerService/CustomerService.jsx
@@ -1,5 +1,25 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-nested-ternary */
-import { Button, CircularProgress, Container, Grid, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  Button,
+  CircularProgress,
+  Container,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  InputLabel,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Typography,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
@@ -13,10 +33,24 @@ function CustomerService() {
   const [scheduleService, setScheduleService] = useState('');
   const [isProcessing, setIsProcessing] = useState(false);
   const [scheduleStatus, setScheduleStatus] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState('');
+  const [selectedQuantity, setSelectedQuantity] = useState(0);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [serviceCost, setServiceCost] = useState(0);
+  const [totalConsumption, setTotalConsumption] = useState(0);
   const navigate = useNavigate();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const scheduleId = params.get('scheduleId');
+
+  // Array de objetos fake para simular os produtos disponíveis
+  const fakeProducts = [
+    { id: 1, name: 'Café', price: 5 },
+    { id: 2, name: 'Água', price: 3 },
+    { id: 3, name: 'Refrigerante', price: 8 },
+    { id: 4, name: 'Salgadinho', price: 7 },
+  ];
 
   useEffect(() => {
     if (scheduleId) {
@@ -28,6 +62,7 @@ function CustomerService() {
           setScheduledTime(scheduleData.time);
           setScheduledDate(scheduleData.date);
           setScheduleService(scheduleData.serviceName);
+          setServiceCost(scheduleData.serviceCost); // Definir o custo do serviço
           setScheduleStatus(scheduleData.status);
         })
         .catch((error) => {
@@ -37,16 +72,39 @@ function CustomerService() {
     } else {
       navigate('/agenda');
     }
-  }, [location.search, navigate]);
+  }, [location.search, navigate, scheduleId]);
 
+  // Função para adicionar produto ao carrinho
+  const handleAddToCart = () => {
+    if (selectedProduct && selectedQuantity > 0) {
+      const productToAdd = fakeProducts.find((product) => product.id === selectedProduct);
+      setSelectedProducts([
+        ...selectedProducts,
+        { product: productToAdd, quantity: selectedQuantity },
+      ]);
+      setSelectedProduct('');
+      setSelectedQuantity(0);
+    }
+  };
+
+  // Função para remover produto do carrinho
+  const handleRemoveFromCart = (index) => {
+    const updatedProducts = [...selectedProducts];
+    updatedProducts.splice(index, 1);
+    setSelectedProducts(updatedProducts);
+  };
+
+  // Função para iniciar o atendimento
   const handleStartService = async () => {
     setIsProcessing(true);
     try {
       const updatedSchedule = await updateSchedule(scheduleId, {
         status: 'Em Andamento',
         started_service: new Date(),
+        payment_method: paymentMethod, // Adiciona a forma de pagamento ao iniciar o serviço
       });
       console.log('Atendimento iniciado:', updatedSchedule);
+      setScheduleStatus('Em Andamento'); // Atualizar o status do botão
       setIsProcessing(false);
     } catch (error) {
       console.error('Erro ao iniciar o atendimento:', error);
@@ -54,6 +112,7 @@ function CustomerService() {
     }
   };
 
+  // Função para finalizar o atendimento
   const handleFinishService = async () => {
     setIsProcessing(true);
     try {
@@ -69,10 +128,25 @@ function CustomerService() {
     }
   };
 
+  // Função para editar o atendimento
   const handleEditService = () => {
     // Adicione aqui a lógica para redirecionar para a página de edição de atendimento
     navigate(`/editar-atendimento/${scheduleId}`);
   };
+
+  // Função para controlar a mudança na forma de pagamento
+  const handlePaymentMethodChange = (event) => {
+    setPaymentMethod(event.target.value);
+  };
+
+  // Calcular o total do consumo
+  useEffect(() => {
+    const total = selectedProducts.reduce(
+      (totalItem, item) => totalItem + item.product.price * item.quantity,
+      0
+    );
+    setTotalConsumption(total);
+  }, [selectedProducts]);
 
   return (
     <>
@@ -100,8 +174,109 @@ function CustomerService() {
             Hora do Corte: {scheduledTime} - Data do Corte: {scheduledDate}
           </Typography>
           <Typography variant="body1" style={{ marginBottom: '1rem' }}>
-            Serviço: {scheduleService}
+            Serviço: {scheduleService} - Valor do Serviço: R$ {serviceCost}
           </Typography>
+          {scheduleStatus !== 'Agendado' && (
+            <>
+              <FormControl fullWidth style={{ marginBottom: '1rem' }}>
+                <InputLabel id="product-label">Produto</InputLabel>
+                <Select
+                  labelId="product-label"
+                  id="product-select"
+                  value={selectedProduct}
+                  onChange={(e) => setSelectedProduct(e.target.value)}
+                >
+                  <MenuItem value="">
+                    <em>Selecione um produto</em>
+                  </MenuItem>
+                  {fakeProducts.map((product) => (
+                    <MenuItem key={product.id} value={product.id}>
+                      {product.name} - R${product.price}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl fullWidth style={{ marginBottom: '1rem' }}>
+                <InputLabel id="quantity-label">Quantidade</InputLabel>
+                <Select
+                  labelId="quantity-label"
+                  id="quantity-select"
+                  value={selectedQuantity}
+                  onChange={(e) => setSelectedQuantity(e.target.value)}
+                >
+                  {[...Array(5).keys()].map((quantity) => (
+                    <MenuItem key={quantity} value={quantity}>
+                      {quantity}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleAddToCart}
+                fullWidth
+                style={{ marginBottom: '1rem' }}
+              >
+                Adicionar ao Carrinho
+              </Button>
+            </>
+          )}
+          {selectedProducts.length > 0 ? (
+            <>
+              <Typography variant="h6" style={{ marginBottom: '1rem' }}>
+                Carrinho:
+              </Typography>
+              <List>
+                {selectedProducts.map((item, index) => (
+                  <ListItem key={index}>
+                    <ListItemText
+                      primary={`${item.product.name} - R$${item.product.price} x ${item.quantity}`}
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => handleRemoveFromCart(index)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                ))}
+              </List>
+              <Typography variant="subtitle1" style={{ marginTop: '1rem' }}>
+                Valor do Consumo: R$ {totalConsumption}
+              </Typography>
+              <Typography variant="subtitle1">
+                Total: R$ {serviceCost + totalConsumption}
+              </Typography>
+            </>
+          ) : (
+            <Typography variant="subtitle1" style={{ marginTop: '1rem' }}>
+              Carrinho vazio
+            </Typography>
+          )}
+          {scheduleStatus !== 'Agendado' && (
+            <>
+              <Typography variant="h6" style={{ marginTop: '1rem' }}>
+                Forma de Pagamento:
+              </Typography>
+              <FormControl component="fieldset" style={{ marginBottom: '1rem' }}>
+                <RadioGroup
+                  aria-label="forma-pagamento"
+                  name="forma-pagamento"
+                  value={paymentMethod}
+                  onChange={handlePaymentMethodChange}
+                >
+                  <FormControlLabel value="PIX" control={<Radio />} label="PIX" />
+                  <FormControlLabel value="Dinheiro" control={<Radio />} label="Dinheiro" />
+                  <FormControlLabel value="Débito" control={<Radio />} label="Débito" />
+                  <FormControlLabel value="Crédito" control={<Radio />} label="Crédito" />
+                </RadioGroup>
+              </FormControl>
+            </>
+          )}
           {isProcessing ? (
             <Button disabled fullWidth style={{ marginTop: '1rem' }}>
               <CircularProgress size={24} /> Processando...
