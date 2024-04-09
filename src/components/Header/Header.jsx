60badcdb94d5b49f41c18ec9df6fc4783c391cae
Guilherme José <gui_guifonseca@hotmail.com>
2024-04-09 09:08:33 -0300

    Adicionado "h" no relógio

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Clock, StyledHeader, Title } from './HeaderStyled';
 
 function Header({ title }) {
   const currentDate = new Date();
-  const formattedDate = `${currentDate.toLocaleDateString('pt-BR', { weekday: 'long' })}, ${currentDate.getHours()}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
+  const formattedDate = `${currentDate.toLocaleDateString('pt-BR', { weekday: 'long' })}, ${currentDate.getHours()}:${currentDate.getMinutes().toString().padStart(2, '0')}h`;
 
   return (
     <StyledHeader>
