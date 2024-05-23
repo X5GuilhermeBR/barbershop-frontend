c8aa76602618233822f7f1d6a2c04bc16ad63d0f
Guilherme José <gui_guifonseca@hotmail.com>
2024-05-23 00:09:32 -0300

    Ajustado label com horários de encaixe.

--- src/utils/generalFunctions.js
+++ src/utils/generalFunctions.js
@@ -16,13 +16,14 @@ export const getFutureDate = () => {
     return `${year}-${month}-${day}`;
   };
 
-const generateAvailableHours = (appointmentType) => {
-  const baseHours = ['9:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
-  if (appointmentType === 'Encaixe') {
-    return baseHours.map(hour => `${hour.charAt(0)}:30`);
-  }
-  return baseHours;
-};
+  const generateAvailableHours = (appointmentType) => {
+    const baseHours = ['9:00', '10:00', '11:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'];
+    if (appointmentType === 'Marcado') {
+      return baseHours;
+    }
+    return ['9:30', '10:30', '11:30', '13:30', '14:30', '15:30', '16:30', '17:30', '18:30', '19:30'];
+  };
+  
   
   export const getDisabledHours = (schedule, appointmentType) => {
     const availableHours = generateAvailableHours(appointmentType);
